9acaa63b6eba6165db4eeb34f4f4a20b3de820de
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-28 10:53:39 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -32,7 +32,7 @@ test = test.merge(event_type_data, on = "event_type")
 test = test.sort_values(["msisdn", "tstamp"], ascending = False)
 test["lat_prev"] = test.groupby(["msisdn"])["lat"].shift(-1)
 test["long_prev"] = test.groupby(["msisdn"])["long"].shift(-1)
-
+get_g
 
 test.loc[~test["lat_prev"].isnull(),:].apply(lambda x: geodesic((x["lat"], x["long"]), (x["lat_prev"], x["long_prev"])).meters, axis = 1)
 

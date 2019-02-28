dece4164d4209b80ebf833c09d92725da526c64e
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-28 11:28:12 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -36,7 +36,9 @@ test["long_prev"] = test.groupby(["msisdn"])["long"].shift(-1)
 
 test.loc[~test["lat_prev"].isnull(),"previous_status_distance"] = test.loc[~test["lat_prev"].isnull(),:].\
                                         apply(lambda x: geodesic((x["lat"],
-                                                                    x["long"]), (x["lat_prev"], x["long_prev"])).meters, axis = 1)
+                                                                    x["long"]),
+                                                                (x["lat_prev"],
+                                                                x["long_prev"])).meters, axis = 1)
 
 
 

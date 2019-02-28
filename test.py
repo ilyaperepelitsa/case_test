e1668b79cf16055ff372a6801b8e183690326a7c
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-28 17:45:30 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -59,9 +59,8 @@ test["segment_lat"], test["segment_lon"] = test.apply(circle_segment, axis = 1)
 
 test["segment_lat"], test["segment_lon"] = test.apply(circle_segment, axis = 1)
 
-a1
- = test.head().apply(circle_segment, axis = 1)
-test.head()
+a1 = test.head().apply(circle_segment, axis = 1)
+a1test.head()
 #
 # test.apply(lambda x: (x["lat"], x["long"]))
 # test.apply(lambda x: print(x["lat"]))

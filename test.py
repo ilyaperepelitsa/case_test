d9b1bb952ed6969c13939c79cff20ce04f3b92e8
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-28 14:31:45 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ test.loc[~test["lat_prev"].isnull(),"previous_status_distance"] = test.loc[~test
 
 
 
-test["tstamp"].dt.date
+test test["tstamp"].dt.date
 test.apply(lambda x: (x["lat"], x["long"]))
 test.apply(lambda x: print(x["lat"]))
 

6050691e31d52cb53d01555bddf9b43268ec50a9
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-28 15:28:39 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -48,7 +48,7 @@ test["date"] = test["tstamp"].dt.date
 
 def circle_segment(data):
     origin = geopy.Point(data["lat"], data["long"])
-    destination = VincentyDistance(meters=data["max_dist"]).destination(origin, b)
+    destination = VincentyDistance(meters=data["max_dist"]).destination(origin, "star")
 
 lat2, lon2 = destination.latitude, destination.longitude
 

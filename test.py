abe4b2c82b49fa9868c2fdc0a41cc00232ab6e5f
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-28 19:47:11 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -74,6 +74,7 @@ test.lac.describe()
 
 x = test.drop_duplicates("cid").loc[61811,"long"]
 y = test.drop_duplicates("cid").loc[61811,"lat"]
+angle 
 x_min = x * 0.95
 x_max = x * 1.05
 y_min = y * 0.95

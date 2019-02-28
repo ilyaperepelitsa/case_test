e70999ef20ee6f7acc9ae454760a5f12880f38ad
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-28 15:27:44 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -55,7 +55,7 @@ lat2, lon2 = destination.latitude, destination.longitude
 
 
 test.columns
-test.drop([cid, msis], axis = 1)
+test.drop([cid, msisdn, imei, event_type, ], axis = 1)
 #
 # test.apply(lambda x: (x["lat"], x["long"]))
 # test.apply(lambda x: print(x["lat"]))

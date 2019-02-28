bec24adc72089d5b54ecf90ae4d5a5c015719188
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-28 19:48:33 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -75,8 +75,8 @@ test.lac.describe()
 x = test.drop_duplicates("cid").loc[61811,"long"]
 y = test.drop_duplicates("cid").loc[61811,"lat"]
 angle = test.drop_duplicates("cid").loc[61811,"end_angle"] - test.drop_duplicates("cid").loc[61811,"start_angle"]
-test.drop_duplicates("cid").loc[61811,"start_angle"]
-
+start_angle = test.drop_duplicates("cid").loc[61811,"start_angle"]
+test.drop_duplicates("cid").loc[61811,"end_angle"]
 x_min = x * 0.95
 x_max = x * 1.05
 y_min = y * 0.95

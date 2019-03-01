73d5a744edb01492a83f7afc2b7286c6e0da1842
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-01 17:46:40 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -152,7 +152,7 @@ for x, i in test.drop_duplicates("cid").loc[test.lac == test.lac.unique()[3],:].
 # pac = mpatches.Wedge(center = [x, y], r = radius, theta1=start_angle + 90, theta2=end_angle)
 # pac = mpatches.Wedge(center = [x, y], r = radius, theta1=0, theta2=15)
 # pac_2 = mpatches.Wedge(center = [x + 0.1, y + 0.1], r = radius, theta1=0, theta2=35)
-    pac_2 = mpatches.Wedge(center = [x, y], r = radius, theta1=start_angle, theta2=math.radians(end_angle), alpha = 0.1)
+    pac_2 = mpatches.Wedge(center = [x, y], r = radius, theta1=math.radians(start_angle), theta2=math.radians(end_angle), alpha = 0.1)
 # plt.gca().add_patch(pac)
     plt.gca().add_patch(pac_2)
     pac_2.set_color('cyan')

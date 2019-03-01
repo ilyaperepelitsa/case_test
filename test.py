8c92ad999f010ba08875a291645a9286508ac0c9
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-01 14:39:49 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -177,7 +177,8 @@ plt.show()
 
 test.drop_duplicates("cid")
 
-plt.scatter(test.drop_duplicates("cid").loc[:,["sector_centroid_lat","sector_centroid_lon"]])
+plt.scatter(test.drop_duplicates("cid").loc[:,["sector_centroid_lat","sector_centroid_lon"]],
+                )
 
 
 test.head().loc[:,["long", "sector_centroid_lon", "lat", "sector_centroid_lat", "plot_radius"]]

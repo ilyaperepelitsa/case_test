91ff693b1ab262ef13ef68792125dc49287479c0
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-01 14:35:58 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -175,6 +175,8 @@ plt.scatter(x = test.tail()["sector_centroid_lon"], y = test.tail()["sector_cent
 plt.axis('equal')
 plt.show()
 
+te
+
 
 test.head().loc[:,["long", "sector_centroid_lon", "lat", "sector_centroid_lat", "plot_radius"]]
 

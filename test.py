2c623d4e8ed26e376dd993e80dbfedc84844d39c
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-02 23:54:50 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -153,11 +153,11 @@ radius = i["plot_radius"]
 # pac = mpatches.Wedge(center = [x, y], r = radius, theta1=start_angle + 90, theta2=end_angle)
 # pac = mpatches.Wedge(center = [x, y], r = radius, theta1=0, theta2=15)
 # pac_2 = mpatches.Wedge(center = [x + 0.1, y + 0.1], r = radius, theta1=0, theta2=35)
-pac_2 = mpatches.Wedge(center = [x, y], r = radius, theta1=math.radians(start_angle), theta2=math.radians(end_angle), alpha = 0.9)
+pac_2 = mpatches.Wedge(center = [x, y], r = radius, theta1=math.radians(start_angle), theta2=math.radians(end_angle), alpha = 0.7)
 # plt.gca().add_patch(pac)
 plt.gca().add_patch(pac_2)
 pac_2.set_color('cyan')
-# plt.scatter(i["sector_centroid_lon"], i["sector_centroid_lat"])
+plt.scatter(i["sector_centroid_lon"], i["sector_centroid_lat"])
 plt.axis('equal')
 plt.show()
 

734c7c039cf351ee0d4dd17be4709221e13a1d1c
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-03 23:46:08 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ import matplotlib.patches as mpatches
 # import matplotlib.pyplot as plt
 import math
 
+import os
 pd.set_option('display.float_format', lambda x: '%.7f' % x)
 
 
@@ -224,7 +225,7 @@ for index, combo in enumerate(list(list(i) for i in combinations(test['msisdn'].
     # print(test.loc[test['msisdn']==combo[0],:].shape, test.loc[test['msisdn'] == combo[1],:].shape)
     event_frame = test.loc[test['msisdn'].isin(combo),:].copy()
     event_frame.loc[:,"combo_id"] = index
-    event_frame.to
+    event_frame.to_csv
     # stack_events = pd.concat([stack_events, event_frame], axis = 0)
     # print(event_frame.shape)
 

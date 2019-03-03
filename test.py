a64eaaf2c4b0241eda6825744d3b3127eea118e0
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-03 16:17:26 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -237,7 +237,7 @@ plt.figure(figsize=(20,10))
 # cmap = get_cmap(test.drop_duplicates("cid").shape[0])
 # new_cmap = rand_cmap(100, type='bright', first_color_black=True, last_color_black=False, verbose=True)
 # len(new_cmap)
-for ix, i in test.drop_duplicates("cid").loc[test.drop_duplicates("cid").index[50:60],:].iterrows():
+for ix, i in test.drop_duplicates("cid").loc[test.drop_duplicates("cid").index[70:60],:].iterrows():
 # i = test.loc[test.index[900],:]
     # print(i["cid"])
     x = i["long"]

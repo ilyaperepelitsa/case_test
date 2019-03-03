23814eacdff266ac1e3945fdaa6f5f42c8bf3563
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-03 23:45:55 +0300

    Updated .DS_Storetest.py .

--- test.py
+++ test.py
@@ -224,7 +224,7 @@ for index, combo in enumerate(list(list(i) for i in combinations(test['msisdn'].
     # print(test.loc[test['msisdn']==combo[0],:].shape, test.loc[test['msisdn'] == combo[1],:].shape)
     event_frame = test.loc[test['msisdn'].isin(combo),:].copy()
     event_frame.loc[:,"combo_id"] = index
-    stack_events = pd.concat([stack_events, event_frame], axis = 0)
+    # stack_events = pd.concat([stack_events, event_frame], axis = 0)
     # print(event_frame.shape)
 
 stack_events.shape

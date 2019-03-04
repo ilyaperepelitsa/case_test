d3b1e1cec2603880985a5c8f72418d158175ee3f
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-04 16:41:59 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -298,8 +298,8 @@ stack_events.columns
 
 lc = LineCollection([stack_events.loc[((stack_events.combo_id == id_id) & (stack_events.tstamp.dt.date == date)),["long", "lat"]] for date in stack_events.tstamp.dt.date.unique()])
 
-for patch in boxes["boxes"]:
-        patch.set_facecolor(cmap())
+# for patch in boxes["boxes"]:
+#         patch.set_facecolor(cmap())
 plt.gca().add_collection(lc)
 
 plt.plot(stack_events.loc[stack_events.combo_id == id_id,:]["sector_centroid_lon"],

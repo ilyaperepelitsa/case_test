cf7694995dd50184b2e821e237ab74949add0e33
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-04 12:01:49 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -246,7 +246,7 @@ stack_events_clean.columns
 
 stack_events_clean["msisdn_lag"] = stack_events_clean.groupby(["msisdn"])["msisdn"].shift(-1)
 stack_events_clean["tstamp_lag"] = stack_events_clean.groupby(["tstamp"])["tstamp"].shift(-1)
-stack_events_clean["tstamp_lag"] = stack_events_clean.groupby(["tstamp"])["tstamp"].shift(-1)
+stack_events_clean["sector_centroid_"] = stack_events_clean.groupby(["tstamp"])["tstamp"].shift(-1)
 stack_events_clean["tstamp_lag"] = stack_events_clean.groupby(["tstamp"])["tstamp"].shift(-1)
 
 test["lat_prev"] = test.groupby(["msisdn"])["lat"].shift(-1)

174fb77df75d7601d51b73a6557b046563bcb596
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-04 12:44:57 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -230,7 +230,7 @@ for index, combo in enumerate(list(list(i) for i in combinations(test['msisdn'].
 
     event_frame = event_frame.drop(['lac', 'cid', 'imei', 'max_dist', 'event_description',
                 'lat_prev', 'long_prev', 'previous_timestamp', 'previous_status_distance',
-                'segment_lat', 'segment_lon', 'plot_radius',
+                'segment_lat', 'segment_lon',
                 'station_angle', 'sector_centroid_shift'], axis = 1).\
                 sort_values(['combo_id', 'tstamp'], ascending = False)
 

e307ae9ca6ffb01302d7b30f18766db428775dd7
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-04 18:45:55 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -270,7 +270,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 pew = TfidfVectorizer()
 pew.fit_transform(stack_events.loc[stack_events.combo_id == id_id,"vendor"].dropna())
-
+pew.fit_transform(
 stack_events.loc[stack_events.combo_id == id_id,"vendor"].str.cat(sep = " ")
 
 

026492f0f557554179ee5ef068f1026ee8b4f394
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-04 18:38:26 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -269,7 +269,7 @@ stack_events.shape
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 pew = TfidfVectorizer()
-pew.fit_transform(stack_events.loc[stack_events.combo_id == id_id,"vendor"].dropna())
+pew.fit_transform(stack_events.loc[stack_events.combo_id == id_id,"vendor"].dropna()).tolist()
 
 dir(pew)
 pew.get_feature_names()

9a50bcd674e94f66ba4bb957e1187ea579022b64
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-04 12:02:39 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -245,9 +245,9 @@ stack_events_clean = stack_events.drop(['lac', 'cid', 'imei', 'long', 'lat', 'ma
 stack_events_clean.columns
 
 stack_events_clean["msisdn_lag"] = stack_events_clean.groupby(["combo_id"])["msisdn"].shift(-1)
-stack_events_clean["tstamp_lag"] = stack_events_clean.groupby(["msisdn"])["tstamp"].shift(-1)
-stack_events_clean["sector_centroid_lat_lag"] = stack_events_clean.groupby(["tstamp"])["tstamp"].shift(-1)
-stack_events_clean["sector_centroid_lat_lag"] = stack_events_clean.groupby(["tstamp"])["tstamp"].shift(-1)
+stack_events_clean["tstamp_lag"] = stack_events_clean.groupby(["combo_id"])["tstamp"].shift(-1)
+stack_events_clean["sector_centroid_lat_lag"] = stack_events_clean.groupby(["combo_id"])["tstamp"].shift(-1)
+stack_events_clean["sector_centroid_lat_lag"] = stack_events_clean.groupby(["combo_id"])["tstamp"].shift(-1)
 
 test["lat_prev"] = test.groupby(["msisdn"])["lat"].shift(-1)
 test["long_prev"] = test.groupby(["msisdn"])["long"].shift(-1)

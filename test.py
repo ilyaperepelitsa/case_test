e967c5e587efc22219d7411fa7312349bb656e8d
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-04 11:23:57 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -240,9 +240,9 @@ stack_events_clean = stack_events.drop(['lac', 'cid', 'imei', 'long', 'lat', 'ma
             'lat_prev', 'long_prev', 'previous_timestamp', 'previous_status_distance',
             'segment_lat', 'segment_lon', 'plot_radius', 'plot_radius_2',
             'station_angle', 'sector_centroid_shift'], axis = 1).\
-            sort_values(['combo_id', 'tstamp'], ascend)
+            sort_values(['combo_id', 'tstamp'], ascending = False)
 
-stack_events_clean.loc[stack_events_clean["combo_id"] == 0,:]
+stack_events_clean.loc[stack_events_clean["combo_id"] == 0,"msi"]
 
 list(set([ tuple(set(i)) for i in combinations(list_stuff, 2) ]))
 

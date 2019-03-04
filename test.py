861b8a0f89b08c0bd2830d23b7c022e3c536f39c
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-04 18:17:01 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -268,7 +268,8 @@ stack_events.shape
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-TfidfVectorizer.fit_transform(stack_events.loc[stack_events.combo_id == id_id,"vendr"])
+pew = 
+TfidfVectorizer.fit_transform(stack_events.loc[stack_events.combo_id == id_id,"vendor"])
 
 stack_events.columns
 

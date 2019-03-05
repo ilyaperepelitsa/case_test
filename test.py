10dde97ef0f1b4491050801a2f3c41f1ae04d3ee
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-05 17:40:19 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -263,8 +263,8 @@ for index, combo in enumerate(list(list(i) for i in combinations(test['msisdn'].
     event_frame["type_all"] = event_frame["type"].str.cat(sep = ", ")
     event_frame["event_type_all"] = event_frame["event_description"].str.cat(sep = ", ")
 
-    event_frame["lat_change"] = event_frame["sector_centroid_lat"] - event_frame["sector_centroid_lat_lag"]
-    event_frame["lat_change"] = event_frame["sector_centroid_lat"] - event_frame["sector_centroid_lat_lag"]
+    # event_frame["lat_change"] = event_frame["sector_centroid_lat"] - event_frame["sector_centroid_lat_lag"]
+    # event_frame["lat_change"] = event_frame["sector_centroid_lat"] - event_frame["sector_centroid_lat_lag"]
 
     event_frame.loc[~event_frame["lat_prev"].isnull(),"path_traveled"] = event_frame.loc[~event_frame["lat_prev"].isnull(),:].\
                                             apply(lambda x: geodesic((x["sector_centroid_lat"],

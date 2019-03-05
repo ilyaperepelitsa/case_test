e91171321ad7aa4dcc7bd3fb277708433e6a3e4a
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-05 17:43:09 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -272,7 +272,7 @@ for index, combo in enumerate(list(list(i) for i in combinations(test['msisdn'].
                                                                     (x["sector_centroid_lat_lag"],
                                                                     x["sector_centroid_lon_lag"])).meters, axis = 1)
 
-    event_frame["path_hours"] = (event_frame['tstamp'] - event_frame['tstamp_lag Date']).dt.hours
+    event_frame["path_hours"] = (event_frame['tstamp'] - event_frame['tstamp_lag']).dt.days
 
     stack_events = pd.concat([stack_events, event_frame], axis = 0)
 

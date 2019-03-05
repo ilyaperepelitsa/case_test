de965bf6cc174ff4f2d91c2fb0258c33b0b1fc6b
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-05 17:32:30 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -266,13 +266,13 @@ for index, combo in enumerate(list(list(i) for i in combinations(test['msisdn'].
     event_frame["lat_change"] = event_frame["sector_centroid_lat"] - event_frame["sector_centroid_lat_lag"]
     event_frame["lat_change"] = event_frame["sector_centroid_lat"] - event_frame["sector_centroid_lat_lag"]
 
-    event_frame.loc[~event_frame["lat_prev"].isnull(),"previous_status_distance"] = event_frame.loc[~event_frame["lat_prev"].isnull(),:].\
+    event_frame.loc[~event_frame["lat_prev"].isnull(),"path_tr"] = event_frame.loc[~event_frame["lat_prev"].isnull(),:].\
                                             apply(lambda x: geodesic((x["sector_centroid_lat"],
                                                                         x["sector_centroid_lon"]),
                                                                     (x["sector_centroid_lat_lag"],
                                                                     x["sector_centroid_lon_lag"])).meters, axis = 1)
 
-                                                
+    (df_test['First_Date'] - df_test['Second Date']).dt.days
 
     stack_events = pd.concat([stack_events, event_frame], axis = 0)
     # print(event_frame.shape)

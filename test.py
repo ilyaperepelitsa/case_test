1b6617f72176affd860e24aba68bf13f9fbd171a
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-03-05 17:03:44 +0300

    Updated test.py.

--- test.py
+++ test.py
@@ -266,7 +266,7 @@ for index, combo in enumerate(list(list(i) for i in combinations(test['msisdn'].
     event_frame["lat_change"] = event_frame["sector_centroid_lat"] - event_frame["sector_centroid_lat_lag"]
     event_frame["lat_change"] = event_frame["sector_centroid_lat"] - event_frame["sector_centroid_lat_lag"]
 
-    test.loc[~test["lat_prev"].isnull(),"previous_status_distance"] = test.loc[~test["lat_prev"].isnull(),:].\
+    event_frame.loc[~event_frame["lat_prev"].isnull(),"previous_status_distance"] = test.loc[~test["lat_prev"].isnull(),:].\
                                             apply(lambda x: geodesic((x["lat"],
                                                                         x["long"]),
                                                                     (x["lat_prev"],

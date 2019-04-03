08a534c3df1ac946bbf0155b7483f37c822cba78
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-04-03 15:20:37 +0300

    Updated sparktest.py.

--- sparktest.py
+++ sparktest.py
@@ -1,9 +1,10 @@
 import findspark from pyspark
+from 
 import SparkContext from pyspark.sql
 import SparkSession
 findspark.init()
 
- sc = SparkContext(appName="MyFirstApp")
-    spark = SparkSession(sc)
-    print("Hello World!")
-    sc.close() #closing the spark session
+sc = SparkContext(appName="MyFirstApp")
+spark = SparkSession(sc)
+print("Hello World!")
+sc.close() #closing the spark session

e76ca1385c28866d7029bd4a52ceb3d6ed2bae97
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-04-04 13:16:51 +0300

    Updated sparktest.py.

--- sparktest.py
+++ sparktest.py
@@ -153,28 +153,6 @@ data_hetero[1]['Porsche']
 #     return rs
 
 
-def extractInformation(row):
-    import re
-    import numpy as np
-
-    selected_indices = [
-         2,4,5,6,7,9,10,11,12,13,14,15,16,17,18,
-         19,21,22,23,24,25,27,28,29,30,32,33,34,
-         36,37,38,39,40,41,42,43,44,45,46,47,48,
-         49,50,51,52,53,54,55,56,58,60,61,62,63,
-         64,65,66,67,68,69,70,71,72,73,74,75,76,
-         77,78,79,81,82,83,84,85,87,89
-    ]
-
-    record_split = re\
-        .compile(
-            r'(\d),' +
-            r'(\d)')
-    try:
-        rs = np.array(record_split.split(row))[selected_indices]
-    except:
-        rs = np.array(['-99'] * len(selected_indices))
-    return rs
 
 
 data_from_file_conv = data_from_file.map(extractInformation)

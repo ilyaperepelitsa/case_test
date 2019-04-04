9aa57bf67aaf2632939dadcac13a3fdddaea1edd
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-04-04 16:21:15 +0300

    Updated domrf_api_test.py.

--- domrf_api_test.py
+++ domrf_api_test.py
@@ -3,7 +3,7 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 
 
-# 
+#
 # PARAMS = {"r": entry["reporter_id"],
 #                   "px": entry["classif"],
 #                   "ps": entry["year"],
@@ -16,10 +16,13 @@ ua = UserAgent()
 #                   "freq": "A",
 #                   "head": "M"
 #                   }
+API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/geoObjects"
+API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/dim_developer_group"
+
 
 headers = requests.utils.default_headers()
 headers['User-Agent'] = ua.random
 
-request_bod = requests.get(url=API_ENDPOINT, params=PARAMS, headers=headers)
-
+request_bod = requests.get(url=API_ENDPOINT, headers=headers)
+request_bod.json()
 # https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/geoObjects
